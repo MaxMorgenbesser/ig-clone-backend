2da014547ac302672227e40d21d9a5bb41ffd662
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-08-17 11:30:08 -0400

    works with post and get

--- src/routers/Photo-Router.ts
+++ src/routers/Photo-Router.ts
@@ -1,10 +1,26 @@
 import { Router } from "express";
 import { Request,Response } from "express";
-import { getAllPhotos } from "../services/photo-services";
+// import { getAllPhotos } from "../services/photo-services";
+import { PhotoServices } from "../services/photo-services";
 export const photoRouter = Router()
 
 photoRouter.get('/', async (req:Request,res:Response)=>{
-const results = await getAllPhotos()
+const results = await PhotoServices.getAllPhotos()
 
 res.status(200).send(results)
+})
+
+photoRouter.post('/', async (req:Request,res:Response)=>{
+    const { photoUrl, description}=req.body
+    if (!photoUrl){
+        res.status(400).send("photo url is missing")
+        return
+    }else if (!description){
+        res.status(400).send("description is missing")
+        return
+    }else{
+    const insertedId = await PhotoServices.createPhoto({photoUrl,description})
+    res.status(200).send(insertedId)
+    }
+
 })
--- src/services/photo-services.ts
+++ src/services/photo-services.ts
@@ -1,18 +1,39 @@
 import { Photo } from "../models/photo";
-import {db} from '../db/db'
+import { db } from "../db/db";
 
-interface PhotoServices{
-    updateLikes(id:string, inc:number):Promise<Photo>
-    createPhoto(photo:Photo):Promise<string>
-    createComment(id:string, comment:string):Promise<Photo>
-    getAllPhotos():Promise<Photo>
+interface PhotoServices {
+//   updateLikes(id: string, inc: number): Promise<Photo>;
+  createPhoto(photo: Photo): Promise<string>;
+//   createComment(id: string, comment: string): Promise<Photo>;
+  getAllPhotos():Promise<Photo[]>;
 }
-
 
 const photoCollections = db.collection<Photo>("photos");
 
-export const getAllPhotos= async():Promise<Photo[]>=>{
- const photos = await photoCollections.find().toArray()
+export const getAllPhotos = async (): Promise<Photo[]> => {
+  const photos = await photoCollections.find().toArray();
 
- return photos
-}
+  return photos;
+};
+
+export const createPhoto = async (photo: Photo): Promise<string> => {
+  
+  try {
+    const res = await photoCollections.insertOne(photo);
+    return res.insertedId.toString();
+  } catch (error) {}
+  return 'Something went wrong'
+};
+
+// export const updateLikes= async(id:string, inc:number=1) Promise <Photo>=>{
+
+// }
+
+// export const createComment = async (id:string, comment:string): Promise<Photo> => {
+//     return 
+
+// }
+export const PhotoServices:PhotoServices=
+{
+    getAllPhotos, 
+    createPhoto, }
